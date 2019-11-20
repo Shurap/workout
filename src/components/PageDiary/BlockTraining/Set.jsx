a1c724903df4ec5c2a8de06d4a3e4f95f30a00af
Shurap <bap.essp@mail.ru>
2019-11-20 23:59:50 +0300

    day is ended

--- src/components/PageDiary/BlockTraining/Set.jsx
+++ src/components/PageDiary/BlockTraining/Set.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import styles from './Set.module.scss'
+import styles from './Set.module.scss';
+import arrow from '../../../images/arrow.png';
 
 const Set = (props) => {
 
@@ -9,10 +10,33 @@ const Set = (props) => {
     setSize(!size)
   }
 
+  const onClickButtonLeftWight = (event) => {
+    event.stopPropagation();
+    console.log('left');
+  }
+
   return (
     <div className={(size) ? styles.setBig : styles.setSmall} onClick={onClick}>
       <div className={styles.wrapperSet}>set {props.number + 1}</div>
-      <div className={styles.wrapperWight}>{props.wight}</div>
+      {
+        (size) ?
+          <div className={styles.wrapperEditButtons}>
+            <img
+              className={styles.buttonLeft}
+              src={arrow} 
+              onClick={onClickButtonLeftWight}
+            />
+            <input
+              className={styles.edit}
+              defaultValue={props.wight}
+            />
+            <img
+              className={styles.buttonRight}
+              src={arrow}
+            />
+          </div> :
+          <div className={styles.wrapperWight}>{props.wight}</div>
+      }
       <div className={styles.wrapperCount}>{props.count}</div>
     </div>
   )
